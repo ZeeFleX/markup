c7d491ed03f4e48727ff9364c2c7d03fdde2ea19
Илья Кардаполов <kardapolov@gmail.com>
2015-02-09 18:34:43 +0500

    Лендинг 4

---
+++ book-3/index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+		<meta charset="utf-8" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet/less" type="text/css" href="less/styles.less">
+        <!-- <link rel="stylesheet" type="text/css" href="css/styles.css"> -->
+        <script src="js/less.min.js"></script>
+</head>
+
+<body>
+   	<section class="intro">
+   		<div class="wrapper">
+   			<div class="right">
+   				<div class="corner"></div>
+   				<p><span class="marked">Получите бесплатно</span><br />видео-семинар<br />Константина Довлатова</p>
+	   			<h3>Бегство от счастья</h3>
+	   			<form action="index.php">
+	   				<div class="field name">
+	   					<input type="text" name="name" placeholder="Ваше имя" />
+	   				</div>
+	   				<div class="field email">
+	   					<input type="text" name="email" placeholder="Ваш email" />
+	   				</div>
+	   				<input type="submit" value="Получить доступ" />
+	   				<p class="endnote">
+	   					Мы гарантируем полную<br />конфиденциальность<br />ваших личных данных
+	   				</p>
+	   			</form>
+	   			<div class="read-more">
+	   				<div class="mouse"></div>
+	   				<p>
+	   					Узнай подробнее о<br />тренере и видео<br/ >далее
+	   				</p>
+	   			</div>
+   			</div>
+   			<div class="left">
+   				<h1>Видео-семинар Константина Довлатова</h1>
+   				<p class="endnote upper">
+   					Профессиональный психолог и долларовый миллионер
+   				</p>
+   				<h2>Бегство от счастья</h2>
+   				<p>Для тех, кто хочет быть здоровым, любимым и богатым уже в этой жизни</p>
+   				<ul>
+   					<li>Как перестать болеть, "психовать" по пустякам и выглядеть старше своего возраста?</li>
+   					<li>Как принять себя без осуждения и перестать обижаться на родителей, супругов и всех остальных?</li>
+   					<li>Как найти любимого человека или вернуть отношениям прежнюю страсть?</li>
+   					<li>Как без особых усилий получать денег в три раза больше, чем сейчас?</li>
+   				</ul>
+   			</div>
+   		</div>
+   	</section>
+   	<section class="about">
+   		<div class="wrapper">
+   			<div class="left">
+   				<p>Об авторе</p>
+   				<h3>Константин Довлатов</h3>
+   				<ul>
+   					<li>Кандидат психологических наук и  основатель Инновационной Школы Коучинга</li>
+   					<li>Тренер по RPT, Духовному выбору и работе с метафорическими картами</li>
+   					<li>Один из самых результативных, востребованных и дорогих тренеров России</li>
+   				</ul>
+   			</div>
+   			<div class="right">
+   				<div class="photo-container">
+   					<img src="images/photos/01.jpg" alt="Константин Довлатов" />
+   				</div>
+   			</div>
+   			<div style="clear:both"></div>
+   			<div class="numbers">
+   				<h2>Цифры</h2>
+   				<div class="item" id="item1">
+   					<div class="icon"></div>
+   					<h3>х 50 раз</h3>
+   					<p class="content">
+   						За 6 месяцев увеличил свой доход в <span class="bold">50 раз</span> (участники его тренингов достигают 2-5-кратного увеличения денежного потока)
+   					</p>
+   				</div>
+   				<div class="item"  id="item2">
+   					<div class="icon"></div>
+   					<h3>> 10 000 решений</h3>
+   					<p class="content">
+   						С помощью методик К. Довлатова <span class="bold">более 10 000 человек</span> решили «нерешаемые» проблемы в бизнесе, финансах, отношениях и здоровье
+   					</p>
+   				</div>
+   				<div class="item"  id="item3">
+   					<div class="icon"></div>
+   					<h3>> 1 000 человек</h3>
+   					<p class="content">
+   						Лично помог <span class="bold">более 1 000 человек</span> радикально изменить свою жизнь
+   					</p>
+   				</div>
+   			</div>
+   		</div>
+   	</section>
+   	<section class="result">
+   		<div class="wrapper">
+   			<div class="magic"></div>
+   			<h2>Что Вы получите в итоге:</h2>
+   			<ul>
+   				<li>
+   					<i class="fa fa-fire-extinguisher"></i>
+   					<div class="content">
+	   					<p class="marked">Спокойствие, легкость и радость</p>
+	   					<p>Спокойствие, легкость и радость, потому что будете управлять своими эмоциями, мыслями и телом. В этом Вам помогут простые техники Константина Довлатова</p>
+   					</div>
+   				</li>
+   				<li>
+   					<i class="fa fa-bolt"></i>
+   					<div class="content">
+	   					<p class="marked">Больше здоровья, энергии и возможностей</p>
+	   					<p>Больше здоровья, энергии и возможностей</p>
+   					</div>
+   				</li>
+   				<div style="clear:both"></div>
+   				<li>
+   					<i class="fa fa-thumbs-o-up"></i>
+   					<div class="content">
+	   					<p class="marked">Деньги, уверенность в себе и признание</p>
+	   					<p>Деньги, уверенность в себе и признание — даже, если Вы уже перестали на это надеяться</p>
+   					</div>
+   				</li>
+   				<li>
+   					<i class="fa fa-graduation-cap"></i>
+   					<div class="content">
+	   					<p class="marked">Ценную информацию</p>
+	   					<p>Ценную информацию, за которую клиенты Константина Довлатова платят немалые деньги — для Вас бесплатно</p>
+   					</div>
+   				</li>
+   				<div style="clear:both"></div>
+   			</ul>
+   		</div>
+   	</section>
+   	<section class="content">
+   		<div class="wrapper">
+   			<div class="left"></div>
+   			<div class="right">
+   				<h2>Краткое содержание видео-семинара</h2>
+   				<ul class="marked plus">
+   					<li>Как быстро и навсегда улучшить свою жизнь, и какие техники для этого использовать (на примере самого автора)</li>
+   					<li>Что записывает подсознание, когда Вы болеете, злитесь, огорчаетесь, или не можете получить желаемое, и чем это опасно</li>
+   					<li>Почему Вас преследуют неприятности, безденежье и разочарование или ПТСР — «бомба замедленного действия»</li>
+   					<li>Скрытые причины Ваших неудач, одиночества и болезней, и какая методика их обезвредит раз и навсегда</li>
+   					<li>Что вернет Вам здоровье, когда врачи опускают руки, или Реанимация по Скайпу</li>
+   					<li>Когда Ваши сознание, эмоции и инстинкты действуют заодно, но вместо пользы наносят Вам вред</li>
+   					<li>Кто «управляет» Вами, когда Вы внезапно увольняетесь с работы, боитесь водить машину или «взрываетесь» по пустякам, и как стать хозяином своего характера</li>
+   					<li>Упражнение, которое ускорит исполнение Ваших желаний, наполнит спокойствием и уверенностью и защитит от новых психологических травм</li>
+   				</ul>
+   			</div>
+   		</div>
+   	</section>
+   	<section class="form">
+   		<div class="wrapper">
+   			<h2>Чтобы получить видео-семинар Константина Довлатова введите свой e-mail в форму ниже</h2>
+   			<form action="index.php">
+   				<div class="field name">
+   					<input type="text" name="name" placeholder="Ваше имя" />
+   				</div>
+   				<div class="field email">
+   					<input type="text" name="email" placeholder="Ваш email" />
+   				</div>
+   				<div style="clear:both"></div>
+   				<input type="submit" value="Получить доступ" />
+   				<p class="endnote">
+   					Мы гарантируем полную конфиденциальность<br />ваших личных данных
+   				</p>
+   			</form>
+   		</div>
+   	</section>
+   	<section class="resume">
+   		<div class="wrapper">
+   			<div>
+	   			<i class="fa fa-quote-right"></i>
+   			</div>
+   			<p class="quote">
+   				<span>
+   					Самое страшное – это не<br />«снова не получается». Самое страшное – это «я<br />больше не хочу пробовать»
+   				</span>
+   				<span class="author">Карлос Кастанеда</span>
+   			</p>
+   		</div>
+   	</section>
+   	<section class="try">
+   		<div class="wrapper">
+   			<a href="index.php" class="button">Попробовать</a>
+   			<p>Пробуйте, потому что в этот раз у Вас точно получится</p>
+   		</div>
+   	</section>
+   	<section class="footer">
+   		<div class="wrapper copyright">
+   			<div class="left">
+   				<p>Copyright © Константин Довлатов 2014 Все права защищены</p>
+   			</div>
+   			<div class="right">
+   				<p class="white">
+   					ИП Довлатов Константин<br />Николаевич
+   				</p>
+   				<p class="ogrn">
+   					ОГРН 309774619400751
+   				</p>
+   			</div>
+   		</div>
+   	</section>
+</body>
+</html>
