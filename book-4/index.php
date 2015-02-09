3148504506bd87462d7b3b20c7d634dc1d262810
Илья Кардаполов <kardapolov@gmail.com>
2015-02-09 19:59:44 +0500

    Лендинг 5

---
+++ book-4/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+		<meta charset="utf-8" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet/less" type="text/css" href="less/styles.less">
+        <!-- <link rel="stylesheet" type="text/css" href="css/styles.css"> -->
+        <script src="js/less.min.js"></script>
+</head>
+
+<body>
+    <section class="page">
+        <div class="wrapper">
+            <span class="start">Открыт секрет массовых неудач в личной жизни, карьере и бизнесе</span>
+            <h1 class="white">
+                ЕСТЬ ВЕЩИ, ИГНОРИРОВАТЬ КОТОРЫЕ<br />
+                <span>ОПАСНО</span> ДЛЯ ЗДОРОВЬЯ
+            </h1> 
+            <div class="player">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Gyag_XQxVts" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <form action="index.php" method="POST">
+                <div class="start">
+                    
+                    <span>Заполните форму ниже и откройте, как быстро</span> <span>наладить свою жизнь:<i class="arrow-container"><div class="arrow1"></div></i></span>
+                </div>
+                <div class="field name">
+                    <input type="text" name="name" placeholder="Ваше имя..."/> 
+                </div>
+                <div class="field email">
+                    <input type="text" name="email" placeholder="Ваш действующий email..."/> 
+                </div>
+                <div class="field submit">
+                    <input class="white" type="submit" value="ПОЛУЧИТЬ КНИГУ" />
+                    <label for="submit">Мы против спама и гарантируем конфиденциальность Ваших личных данных</label>
+                </div>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
