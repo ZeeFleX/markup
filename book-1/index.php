a8d35416ba107937e3b93b85c7d35cf63249f0c7
Илья Кардаполов <kardapolov@gmail.com>
2015-02-09 11:23:45 +0500

    Лендинг 2

---
+++ book-1/index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+		<meta charset="utf-8" />
+        <link href='http://fonts.googleapis.com/css?family=PT+Serif:400,700,400italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet/less" type="text/css" href="less/styles.less">
+        <!-- <link rel="stylesheet" type="text/css" href="css/styles.css"> -->
+        <script src="js/less.min.js"></script>
+</head>
+
+<body>
+    <header class="panel">
+        <p class="white">
+            <i class="fa fa-exclamation-triangle"></i>
+            <span>Открыт секрет массовых неудач в личной жизни, карьере и бизнесе</span>
+            <i class="fa fa-exclamation-triangle"></i>
+        </p>
+    </header>
+    <section class="header">
+        <div class="wrapper">
+            <h1 class="white">
+                ЕСТЬ ВЕЩИ, ИГНОРИРОВАТЬ КОТОРЫЕ<br />
+                <span>ОПАСНО</span> ДЛЯ ЗДОРОВЬЯ
+            </h1> 
+            <form action="index.php" method="POST">
+                <div class="start">
+                    
+                    <span>Заполните форму ниже и откройте, как быстро</span> <span>наладить свою жизнь:<div class="arrow1"></div></span>
+                </div>
+                <div class="field name">
+                    <input type="text" name="name" placeholder="Ваше имя..."/> 
+                </div>
+                <div class="field email">
+                    <input type="text" name="email" placeholder="Ваш действующий email..."/> 
+                </div>
+                <div class="field submit">
+                    <input class="white" type="submit" value="ПОЛУЧИТЬ КНИГУ" />
+                    <label for="submit">Мы против спама и гарантируем конфиденциальность Ваших личных данных</label>
+                </div>
+                <div class="confirm-text white">
+                    <div class="arrow2"></div>
+                    <p>
+                        <span>
+                        <span class="italic">Да, я хочу узнать об этом</span><br /><span class="upper">ПРЯМО СЕЙЧАС!</span>
+                        </span>
+                    </p>
+                </div>
+            </form>
+        </div>
+    </section>
+    <section class="section2">
+        <div class="wrapper">
+            <h1 class="sea">
+                Как долго вы<br />согласны это терпеть?!
+            </h1>
+            <table class="problems">
+                <tr>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>У Вас скучная работа, коллеги-неудачники, начальник-самодур, а зарплату давно не повышали</td>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Ваш бизнес уже не «зажигает», на партнеров надежды нет, на семью времени не остается, отдохнуть некогда, а с друзьями только перезваниваетесь</td>
+                </tr>
+                <tr>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Вы нашли любимое дело, выкладываетесь на 100% в надежде на большие деньги, а от близких слышите: «И когда ты наиграешься и устроишься на нормальную работу?!»</td>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Вы «пашете» и крутитесь как белка в колесе, а доходы не успевают за кредитами, цены постоянно растут, и никто не ценит Ваших усилий</td>
+                </tr>
+                <tr>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Вы выбираете машину «по деньгам», а вовсе не такую же «красавицу», которую паркует Ваш сосед, а ведь она Вам так нравится…</td>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Вам непонятно, как люди ухитряются покупать по три квартиры или недвижимость за границей, когда Вы с семьей живете в «двушке» или «трешке» (хорошо, если без родителей)</td>
+                </tr>
+                <tr>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Вы читаете книги о том, как надо думать, чтобы богатеть, ходите по тренингам и семинарам, занимаетесь разными духовными практиками... Только эффект получаете кратковременный, или его вообще нет</td>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Вам постоянно приходится быть «в форме», улыбаться и делать вид, что у Вас всё в порядке, хотя на душе частенько «кошки скребут», и нет сил притворяться</td>
+                </tr>
+                <tr>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Вы часто ругаете себя за свою лень, глупость, доверчивость и неумение зарабатывать деньги. У Вас «кривые руки», тяжелый характер, лишний вес, нерешительность и одиночество… А еще отсутствие силы воли и, в придачу, «опять забыл маме позвонить»…</td>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Ваша «вторая половинка» давно не воспринимает Вас всерьез. Телевизор заменил разговоры по душам, вы перестали строить совместные планы, а сексом занимаетесь редко, и больше по привычке, чем по страсти</td>
+                </tr>
+                <tr>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Вам приходиться с головой уходить в работу, здоровый образ жизни, «Одноклассники» или другие соцсети… короче, куда угодно, лишь бы отвлечься от своего одиночества</td>
+                    <td class="icon"><img src="images/icons/cross.png" /></td>
+                    <td>Вы выглядите старше своего возраста, недосыпаете, чувствуете хроническую усталость и чувство безысходности от того, что делам нет конца-края. Хочется просто лечь и лежать… пропади оно все пропадом…</td>
+                </tr>
+            </table>
+        </div>
+    </section>
+    <section class="section3">
+        <div class="wrapper">
+            <h1 class="white">
+                <span class="italic pt">И знаете что?</span> Вы не одиноки —
+                абсолютное большинство людей
+                живет в такой же глубокой… <span class="blue_dark">«яме»</span>, и
+                считает, что у них все в порядке.
+            </h1>
+        </div>
+    </section>
+    <section class="section4">
+        <div class="wrapper">
+            <h1 class="upper sea">
+                Почему ваши мечты так и<br />
+                <span class="red">не прeвратились</span> <span class="italic pt lower">в реальность?</span>
+            </h1>
+            <div class="italic pt resume">
+                <div class="arrow3"></div>
+                <p class="bold">Вашей вины здесь нет.</p>
+            </div>
+            <p>
+                Причина большинства проблем — психологические травмы, спрятанные в Вашем подсознании. Более того, многие из них возникли еще <span class="upper sea">до Вашего рождения.</span>
+            </p>
+        </div>
+    </section>
+    <section class="section5">
+        <div class="wrapper">
+            <div class="left">
+                <p>
+                    С момента зачатия будущий ребенок «слышит» все, что происходит во внешнем мире. Его инстинкт самосохранения тщательно записывает все события в подсознание, чтобы потом человек знал, что несет ему пользу, а что вред.
+                </p>
+                <p>
+                    А теперь представьте, что у беременной женщины сильный токсикоз, или после родов у нее нет молока, или это нежеланный ребенок, или… продолжать можно до бесконечности. Расспросите своих родителей (если есть такая возможность), чтобы узнать о том, как это было именно у Вас.
+                </p>
+                <p>
+                    Представляете, какую программу записывает подсознание ребенка в таких ситуациях? Ведь он в этот момент единое целое со своей мамочкой, а значит, — активный «участник» процесса. 
+                </p>
+                <p>
+                    Мы приходим в этот мир таким образом, что избежать травм не удается никому. Разница в том, что кому-то их достается больше, а кому-то меньше.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="section6">
+        <div class="wrapper">
+            <div class="right">
+                <h1 class="sea bold">А что потом?</h1>
+                <p>
+                    Рождение, детство, отрочество, юность… Любой из этих периодов несет новые травмирующие ситуации. Про взрослое состояние и говорить нечего — стресс на стрессе и стрессом погоняет.
+                </p>
+                <p>
+                    Ваш мозг постепенно забывает о неприятных моментах, а вот подсознание о них помнит. Оно тщательно сортирует и создает целые цепочки травм, нанизывая их, как бусины, на ту основу, которую Вы получили еще в утробе матери.
+                </p>
+                <p>
+                    Так, незаметно для себя, Вы начинаете бояться всего нового, потому что оно может оказаться опасным и болезненным. Вы ходите по замкнутому кругу привычных и понятных ситуаций, думаете, что в Вашей жизни все нормально и говорите, что у Вас богатый жизненный опыт.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="section7">
+        <div class="wrapper">
+            <div class="question"></div>
+            <h1 class="bold">
+                Что же вам делать с этим<br />
+                <span class="red bold">«наследством»</span>?
+            </h1>
+            <p>
+                Не отчаивайтесь, не все так безнадежно. Как говорил главный герой одного фильма, <span class="italic">безнадёжно — это когда на крышку гроба падает земля.</span> Остальное можно исправить.
+            </p>
+            <p>
+                Тысячи людей по всему миру доказали своим опытом, что травмы можно убрать навсегда. Даже те, которые получены в дородовом периоде. 
+            </p>
+            <p>
+                Именно об этом – новая книга <span class="bold">Константина Довлатова «Дородовые травмы: миф или суровая реальность?»</span>
+            </p>
+        </div>
+    </section>
+    <section class="section8">
+        <div class="bg-container"></div>
+        <div class="wrapper">
+            <div class="left">
+                <h1 class="white upper bold">КОНСТАНТИН ДОВЛАТОВ</h1>
+                <ul class="white">
+                    <li>Профессиональный психолог, основатель инновационной Школы Коучинга</li>
+                    <li>Входит в ТОП-5 самых высокооплачиваемых коучей Рунета</li>
+                    <li>Помог более 10 000 чел. решить «неразрешимые» проблемы в бизнесе, финансах, отношениях, здоровье, поиске дела жизни.</li>
+                </ul>
+            </div>
+            <div class="right">
+                <div class="photo-container">
+                    <img src="images/photos/dowlatov.jpg" />
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section9">
+        <div class="wrapper">
+            <h1 class="sea upper bold">Из книги вы узнаете</h1>
+            <table>
+                <tr>
+                    <td class="icon">
+                        <div class="number">1</div>
+                    </td>
+                    <td class="text">Кто на самом деле виноват в том, что Вы болеете, сердитесь/обижаетесь на всех, недостаточно зарабатываете и не получаете от жизни никакого удовольствия (и это не Вы сами!)</td>
+                    <td class="icon"><div class="number">2</div></td>
+                    <td class="text">Что незаметно «живет» внутри каждого человека, подтачивает его душевные силы и контролирует поступки (если Вы думаете, что избежали этой участи, то сильно ошибаетесь…)</td>
+                </tr>
+                <tr>
+                    <td class="icon"><div class="number">3</div></td>
+                    <td class="text">Почему иногда «несет» так, что ссора из-за немытой посуды незаметно перерастает в крупный скандал и лишает Вас секса на 2 недели :)</td>
+                    <td class="icon"> <div class="number">4</div></td>
+                    <td class="text">Сколько психических травм Вы получаете к сорока годам жизни, и как от них избавиться</td>
+                </tr>
+                <tr>
+                    <td class="icon"><div class="number">5</div></td>
+                    <td class="text">Три простых шага, которые отделяют Вас от здоровья, богатства и душевного равновесия</td>
+                    <td class="icon"> <div class="number">6</div></td>
+                    <td class="text">Как перестать быть «магнитом» для 33 несчастий</td>
+                </tr>
+                <tr>
+                    <td class="icon"><div class="number">7</div></td>
+                    <td class="text">Какие ключевые моменты Вашего прошлого постоянно «расставляют ловушки» на Вашем пути к здоровью и процветанию</td>
+                    <td class="icon"> <div class="number">8</div></td>
+                    <td class="text">Почему Вы не можете наладить хорошие отношения со своими детьми, а педагогические приемы работают слабо</td>
+                </tr>
+                <tr>
+                    <td class="icon"><div class="number">9</div></td>
+                    <td class="text">Как быстро и гарантированно получить от жизни то, на что Вы уже и не надеетесь: деньги, здоровье, уверенность в себе, любовь и уважение близких, — причем так, чтобы раз и навсегда…</td>
+                    <td class="icon"></td>
+                    <td class="text"></td>
+                </tr>
+            </table>
+        </div>
+        
+    </section>
+    <section class="section10">
+        <div class="head">
+            <div class="wrapper">
+                <h1 class="upper white">
+                    Узнайте всю правду о настоящих причинах
+                    Ваших жизненных неурядиц.
+                    Более того, <span class="">разберитесь с ними раз и навсегда</span>!
+                </h1>
+            </div>
+        </div>
+        <div class="separator"></div>
+        <div class="body">
+            <div class="wrapper">
+                <h2>
+                    Заполните форму ниже, чтобы получить<br />
+                    новую книгу Константина Довлатова <span class="upper red">бесплатно</span>
+                </h2>
+                <form action="index.php" method="POST">
+                    <div class="field name">
+                        <input type="text" name="name" placeholder="Ваше имя..."/> 
+                        <label for="name">Чтобы знать, как к Вам обращаться.</label>
+                    </div>
+                    <div class="field email">
+                        <input type="text" name="email" placeholder="Ваш действующий email..."/> 
+                        <label for="name">На него вы получите книгу.</label>
+                    </div>
+                    <div class="field submit">
+                        <input type="submit" value="ПОЛУЧИТЬ КНИГУ" />
+                        <label for="submit">Мы против спама и гарантируем конфиденциальность Ваших личных данных</label>
+                    </div>
+                </form>
+                <div style="clear:both"></div>
+                
+            </div>
+        </div>
+        <div class="ps">
+            <p class="">
+                <span class="upper red">P.S.</span> Не закрывайте глаза на свои проблемы, когда есть шанс избавиться от них навсегда.
+            </p>
+        </div>
+    </section>
+    <footer>
+        <div class="wrapper">
+            <p class="copyright white">
+                © 2014 РПТ ИНФО  •  Все права защищены
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
