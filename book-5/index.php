2c2e3108421a496cd2cedd57cbf4a8228ea56c58
Илья Кардаполов <kardapolov@gmail.com>
2015-02-10 15:36:52 +0500

    Видео и текстура на утверждение

---
+++ book-5/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+		<meta charset="utf-8" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet/less" type="text/css" href="less/styles.less">
+        <!-- <link rel="stylesheet" type="text/css" href="css/styles.css"> -->
+        <script src="js/less.min.js"></script>
+</head>
+
+<body>
+    <section class="page">
+        <div class="texture">
+            
+        </div>
+        <video autoplay="autoplay" loop="loop" width="100%">
+            <source src="video/video.mp4">
+        </video>
+    </section>
+</body>
+</html>
