9e4f05b22cb81c2d7957df37ede09af04c410d5e
Илья Кардаполов <kardapolov@gmail.com>
2015-02-27 16:18:51 +0500

    Вёрстка календаря

---
+++ consult/module_calendar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+		<?php include_once('_head.php'); ?>
+</head>
+<body class="default-template">
+    <div class="wrapper">
+        <div class="calendar">
+            <header>
+                <a href="#"><i class="fa fa-angle-double-left"></i></a>
+                <a href="#"><i class="fa fa-angle-double-right"></i></a>
+                <p class="month">Январь</p>
+                <div class="clr"></div>
+            </header>
+            <div class="container">
+                <section class="day-of-weeks">
+                    <div class="item">Пн</div>
+                    <div class="item">Вт</div>
+                    <div class="item">Ср</div>
+                    <div class="item">Чт</div>
+                    <div class="item">Пт</div>
+                    <div class="item">Сб</div>
+                    <div class="item">Вс</div>
+                </section>
+                <section class="day-of-month">
+                    <?php
+                        $offset = 2;
+                        $today = 27;
+                        for($i = 1; $i <= $offset; $i++) {
+                    ?>
+                        <span class="item <?php echo 'empty'; ?> <?php if(count_weekend($offset)) echo 'weekend'; ?> "></span>
+                    <?php } ?>
+                    <?php for($i = 1; $i <= 31; $i++) {?>
+                        <a href="#" class="item <?php if(count_weekend($i + $offset)) echo 'weekend'; ?> <?php if($i == $today) echo 'today'; ?>"><?php echo $i; ?></a>
+                    <?php } ?>
+                    <div class="clr"></div>
+                </section>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<?php 
+    function count_weekend($cell){
+        if($cell % 7 == 6 || $cell % 7 == 0) return true;
+    }
+?>
