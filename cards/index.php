8f5b7392010355cf06d61520f5041af86ad771c6
Илья Кардаполов <kardapolov@gmail.com>
2015-03-05 16:18:15 +0300

    Текстовая страница

--- cards/index.php
+++ cards/index.php
@@ -20,7 +20,8 @@
             <li><a href="page_profile.php">Страница профиля терапевта</a></li>
             <li><a href="page_profile_settings_common.php">Терапевт. Настройки профиля (основные)</a></li>
             <li><a href="page_profile_settings_contacts.php">Терапевт. Настройки профиля (контакты)</a></li> 
-            <li><a href="page_profile_settings_security.php">Терапевт. Настройки профиля (Безопасность)</a></li> 
+            <li><a href="page_profile_settings_security.php">Терапевт. Настройки профиля (Безопасность)</a></li>
+            <li><a href="page_text.php">Текстовая страница</a></li>  
         </ol>
         <h1>Полезные ссылки</h1>
         <ol>
