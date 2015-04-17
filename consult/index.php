0c95df176c6e7f1880f59947a56c92dadd0f0bba
Илья Кардаполов <kardapolov@gmail.com>
2015-04-18 00:02:48 +0500

    Рабочее время (страница)

--- consult/index.php
+++ consult/index.php
@@ -21,6 +21,7 @@
             <li><a href="page_profile_settings_common.php">Основные настройки профиля (консультант)</a></li> 
             <li><a href="page_profile_settings_security.php">Страница настроек безопасности (консультант)</a></li>
             <li><a href="page_profile_settings_about_me.php">Страница настроек. Информация о себе (консультант)</a></li>
+            <li><a href="page_profile_settings_worktime.php">Страница настроек. Рабочее время (консультант)</a></li>
             <li><a href="page_profile_settings_contacts.php">Страница контактов. (консультант)</a></li>
             <li><a href="page_profile_settings_consults.php">Страница консультаций. (консультант)</a></li>
             <li><a href="page_profile_schedule.php">Мое расписание. (консультант)</a></li>
---
+++ consult/page_profile_settings_worktime.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+		<?php include_once('_head.php'); ?>
+</head>
+<body class="default-template page-profile-settings worktime">
+    <NOSCRIPT>
+        <div class="no-script-layout">
+            <div class="wrapper">
+                <h1>В настройках вашего браузера отключен JavaScript.</h1>
+                <h2>Чтобы продолжить пользоваться сайтом вам необходимо активировать JavaScript в настройках или скачать современный браузер.</h2>
+            </div>
+        </div>        
+    </NOSCRIPT>
+    <header class="header">
+        <div class="logo"></div>
+        <p class="title">Консультации</p>
+        <div class="profile-snippet">
+            <div class="photo-container">
+                <img src="images/photos/01.jpg" />
+            </div>
+            <div class="info">
+                <div class="name">Иванов Иван</div>
+                <div class="job">Консультант</div>
+            </div>
+            <div class="more-container">
+                <i class="fa fa-caret-down"></i>
+                <div class="dropdown-menu">
+                    <ul>
+                        <a href="#">Выйти</a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="profile-notifications">
+            <div class="events active">
+                <div class="count">3</div>
+                <i class="fa fa-calendar"></i>
+            </div>
+            <div class="new-messages">
+                <div class="count">2</div>
+                <i class="fa fa-envelope"></i>
+            </div>
+            <div class="new-notifications">
+                <div class="count">2</div>
+                <i class="fa fa-bell"></i>
+            </div>
+        </div>
+    </header>
+    <section class="user-menu-section">
+        <div class="wrapper">
+            <div class="user-menu-container">
+                <ul class="user-menu">
+                    <li>
+                        <a href="#">Мое расписание</a>
+                    </li>
+                    <li>
+                        <a href="#">Мои клиенты</a>
+                    </li>
+                    <li>
+                        <a href="#">Личные вопросы</a>
+                    </li>
+                    <li>
+                        <a href="#">Мои уведомления</a>
+                    </li>
+                    <li>
+                        <a href="#">Написать статью</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <div class="wrapper">
+        <section class="content">
+            <h4>Рабочее время</h4>
+            <div class="content-block">
+                <form action="#" method="POST">
+                    <?php
+                        $days = Array('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+                        foreach ($days as $key => $day) {
+                    ?>
+                    <div class="day">
+                        <h3><?php echo $day; ?></h3>
+                        <div class="field begin">
+                            <label for="begin[]">Начало рабочего дня</label>
+                            <select name="begin[]" class="select2">
+                                <option value="00:00" selected="selected">00:00</option>
+                                <option value="00:30">00:30</option>
+                                <option value="01:00">01:00</option>
+                                <option value="01:30">01:30</option>
+                                <option value="02:00">02:00</option>
+                                <option value="02:30">02:30</option>
+                                <option value="03:00">03:00</option>
+                                <option value="03:30">03:30</option>
+                                <option value="04:00">04:00</option>
+                                <option value="04:30">04:30</option>
+                                <option value="05:00">05:00</option>
+                                <option value="05:30">05:30</option>
+                                <option value="06:00">06:00</option>
+                                <option value="06:30">06:30</option>
+                                <option value="07:00">07:00</option>
+                                <option value="07:30">07:30</option>
+                                <option value="08:00">08:00</option>
+                                <option value="08:30">08:30</option>
+                                <option value="09:00">09:00</option>
+                                <option value="09:30">09:30</option>
+                                <option value="10:00">10:00</option>
+                                <option value="10:30">10:30</option>
+                                <option value="11:00">11:00</option>
+                                <option value="11:30">11:30</option>
+                                <option value="12:00">12:00</option>
+                                <option value="12:30">12:30</option>
+                                <option value="13:00">13:00</option>
+                                <option value="13:30">13:30</option>
+                                <option value="14:00">14:00</option>
+                                <option value="14:30">14:30</option>
+                                <option value="15:00">15:00</option>
+                                <option value="15:30">15:30</option>
+                                <option value="16:00">16:00</option>
+                                <option value="16:30">16:30</option>
+                                <option value="17:00">17:00</option>
+                                <option value="17:30">17:30</option>
+                                <option value="18:00">18:00</option>
+                                <option value="18:30">18:30</option>
+                                <option value="19:00">19:00</option>
+                                <option value="19:30">19:30</option>
+                                <option value="20:00">20:00</option>
+                                <option value="20:30">20:30</option>
+                                <option value="21:00">21:00</option>
+                                <option value="21:30">21:30</option>
+                                <option value="22:00">22:00</option>
+                                <option value="22:30">22:30</option>
+                                <option value="23:00">23:00</option>
+                                <option value="23:30">23:30</option>
+                                <option value="00:00">00:00</option>
+                            </select>
+                        </div>
+                        <div class="field end">
+                            <label for="end[]">Конец рабочего дня</label>
+                            <select name="end[]" class="select2">
+                                <option value="00:00" selected="selected">00:00</option>
+                                <option value="00:30">00:30</option>
+                                <option value="01:00">01:00</option>
+                                <option value="01:30">01:30</option>
+                                <option value="02:00">02:00</option>
+                                <option value="02:30">02:30</option>
+                                <option value="03:00">03:00</option>
+                                <option value="03:30">03:30</option>
+                                <option value="04:00">04:00</option>
+                                <option value="04:30">04:30</option>
+                                <option value="05:00">05:00</option>
+                                <option value="05:30">05:30</option>
+                                <option value="06:00">06:00</option>
+                                <option value="06:30">06:30</option>
+                                <option value="07:00">07:00</option>
+                                <option value="07:30">07:30</option>
+                                <option value="08:00">08:00</option>
+                                <option value="08:30">08:30</option>
+                                <option value="09:00">09:00</option>
+                                <option value="09:30">09:30</option>
+                                <option value="10:00">10:00</option>
+                                <option value="10:30">10:30</option>
+                                <option value="11:00">11:00</option>
+                                <option value="11:30">11:30</option>
+                                <option value="12:00">12:00</option>
+                                <option value="12:30">12:30</option>
+                                <option value="13:00">13:00</option>
+                                <option value="13:30">13:30</option>
+                                <option value="14:00">14:00</option>
+                                <option value="14:30">14:30</option>
+                                <option value="15:00">15:00</option>
+                                <option value="15:30">15:30</option>
+                                <option value="16:00">16:00</option>
+                                <option value="16:30">16:30</option>
+                                <option value="17:00">17:00</option>
+                                <option value="17:30">17:30</option>
+                                <option value="18:00">18:00</option>
+                                <option value="18:30">18:30</option>
+                                <option value="19:00">19:00</option>
+                                <option value="19:30">19:30</option>
+                                <option value="20:00">20:00</option>
+                                <option value="20:30">20:30</option>
+                                <option value="21:00">21:00</option>
+                                <option value="21:30">21:30</option>
+                                <option value="22:00">22:00</option>
+                                <option value="22:30">22:30</option>
+                                <option value="23:00">23:00</option>
+                                <option value="23:30">23:30</option>
+                                <option value="00:00">00:00</option>
+                            </select>
+                        </div>
+                    </div>
+                    <?php } ?>
+                    <div class="clr"></div>
+                    <input type="submit" value="Сохранить изменения" class="btn" />
+                    <a href="#" class="btn ghost">Перейти в профиль</a>
+                </form>
+            </div>
+        </section>
+        <aside class="additional-menu">
+            <h4>Настройки профиля</h4>
+            <div class="content-block">
+                <ul>
+                    <li>
+                        <a href="#">Основные настройки</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">Рабочее время</a>
+                    </li>
+                    <li>
+                        <a href="#">Консультации</a>
+                    </li>
+                    <li>
+                        <a href="#">Информация о себе</a>
+                    </li>
+                    <li>
+                        <a href="#">Контакты</a>
+                    </li>
+                    <li>
+                        <a href="#">Партнеры</a>
+                    </li>
+                    <li>
+                        <a href="#">Настройки безопасности</a>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+    </div>
+    <footer class="footer">
+        <section class="library">
+            <div class="wrapper">
+                <div class="title">
+                    <h3>Консультации</h3>
+                </div>
+                <div class="library-list">
+                    <h4>Библиотека</h4>
+                    <div class="items-container">
+                        <a class="item" href="#">Проблемные адаптации</a>
+                        <a class="item" href="#">Развитие способностей</a>
+                        <a class="item" href="#">Послеродовая адаптация</a>
+                        <a class="item" href="#">Личностное развитие</a>
+                        <a class="item" href="#">Развитие карьеры</a>
+                        <a class="item" href="#">Проблемы с памятью</a>
+                        <a class="item" href="#">Возрастные кризисы</a>
+                        <a class="item" href="#">Отказ от курения</a>
+                        <a class="item" href="#">Управление весом</a>
+                        <a class="item" href="#">Самореализация</a>
+                        <a class="item" href="#">Проблема сна</a>
+                        <a class="item" href="#">Тревоги и фобии</a>
+                    </div>
+                </div>
+                <div class="clr"></div>
+            </div>
+        </section>
+        <section class="bottom">
+            <p class="copyright">Copyright 2015 Консультации</p>
+            <ul class="footer-menu">
+                <li>
+                    <a href="#">О сервисе</a>
+                </li>
+                <li>
+                    <a href="#">Политика конфиденциальности</a>
+                </li>
+                <li>
+                    <a href="#">Условия использования</a>
+                </li>
+                <li>
+                    <a href="#">Оферта</a>
+                </li>
+            </ul>
+        </section>
+    </footer>
+</body>
+</html>
