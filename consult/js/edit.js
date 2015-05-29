3bae9f86b91bce44854bc24d1e3608e97e34515a
Илья Кардаполов <kardapolov@gmail.com>
2015-05-29 15:33:45 +0500

    Карандашик на форме редактирования текстов

--- consult/js/edit.js
+++ consult/js/edit.js
@@ -16,11 +16,13 @@
 			var $handler = $('<i class="fa fa-pencil" style="position:absolute; top:0; right:0px; font-size:1.4rem; cursor:pointer; z-index:1; color:#4ad0c5"></i>').prependTo($block);
 			$handler.on('click', function(){
 				if(!$block.find('.editable-form').length){
+					$handler.css('display', 'none');
 					var $form = $('<form class="editable-form" action="' + $block.attr('data-url') + '" style="top:0; left:0; position:absolute; background: #fff; z-index:1; height:' + $block.outerHeight() + 'px; width: ' + ($block.outerWidth() - 20) + 'px;"></form>').prependTo($block).show(300);
 					var $input = $('<textarea name="content" style="width:100%; height:100%; font-size:1.4rem; line-height:135%;" class="allow-clear"></textarea>').prependTo($form);
 					$input.html($.trim(strip_tags($container.html())));
 					var $cancel = $('<i class="fa fa-times" style="position:absolute; top:20px; right:0px; font-size:1.4rem; cursor:pointer; z-index:1; color:#f85649"></i>').prependTo($block);
 					var $submit = $('<i class="fa fa-check" style="position:absolute; top:40px; right:0px; font-size:1.4rem; cursor:pointer; z-index:1; color:#0f9b58"></i>').prependTo($block);	
+
 					$input.focus();
 					$cancel.on('click', function(){
 						$form.remove();
@@ -40,12 +42,14 @@
 						$input.remove();
 						$cancel.remove();
 						$submit.remove();
+						$handler.css('display', 'block');
 					});
 				}else{
 					$form.show(0);
 					$input.show(0);
 					$cancel.show(0);
 					$submit.show(0);
+					$handler.css('display', 'none');
 				}
 			});
 		});
