132ae7eaa79c22a9ef991b057a28b6bab3708054
Юлия Коробейникова <yu.korobeinikova@gmail.com>
2015-08-12 13:09:53 +0500

    100 дней

---
+++ letter100days/letter100days.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+	</head>
+	<body style="padding:0; margin:0; font-family:Arial, sans-serif">
+		<div class="container" style="width: 685px;">
+			<div class="top" style="background: url(bg-top.jpg) no-repeat; height: 505px;">
+				<div class="title" style="font-family: sans-serif; color: #ffffff; font-size: 16px; font-weight: 400; text-align: center; line-height: 48px; padding-top: 20px;">
+					Проект «100 трансформаций»
+				</div>
+				<div class="sale" style="background: url(sale.png) no-repeat; height: 240px; margin-top: 65px; margin-left: 260px;"></div>
+				<div class="hello" style="font-family: sans-serif; color: #ffffff; font-size: 28px; font-weight: 400; line-height: 12px; text-align: right; margin-top: 35px; margin-right: 50px;">Здравствуйте!<br><br><br><span style="font-size: 18px; line-height: 12px;">На связи Константин Довлатов</span></div>
+			</div>
+			<p style="font-family: sans-serif; color: #5c6770; font-size: 16px; font-weight: 400; line-height: 22px; text-align: center; margin: 40px 60px 0 60px;">
+				На днях я провёл небольшой опрос о ценности реинтеграций. Огромное спасибо всем участникам – он оказался неожиданно важным. <br><br>Я увидел, что многие хотят (а значит – могут) получать от реинтеграций пользу минимум на десятки тысяч рублей. Им мешает только одно. <br><br>Прямо сейчас у них нет возможности проходить «100 дней» с реинтеграциями.
+			</p>
+			<h1 style="font-family: sans-serif; color: #fa3b48; font-size: 22px; font-weight: 400; line-height: 18px; text-align: center; margin-top: 40px;">Но сейчас она появилась!</h1>
+			<a href="#"class="button-1" style="display: block; background: url(buttonn.png); width:604px; height:63px; border-radius: 8px; -moz-border-radius: 8px; -o-border-radius: 8px; -webkit-border-radius: 8px; text-decoration: none; box-sizing:border-box; -moz-box-sizing:border-box; border-bottom:solid 2px #5c8a1a; font-family: sans-serif; color:  #ffffff; font-size: 18px; font-weight: 400; line-height: 18px; text-align: center; margin: 20px auto; padding-top: 23px;">Воспользуйтесь своим шансом пройти все реинтеграции</a>
+			<div class="results" style="background: url(bg-results.png) no-repeat; margin-top: 30px; padding-top: 55px; padding-bottom: 40px;">
+				<p style="font-family: sans-serif; color: #5c6770; font-size: 16px; font-weight: 400; line-height: 22px; text-align: center; margin: 0 80px;">Я хочу, чтобы реинтеграции помогали как можно большему числу людей. Созданы они именно для этого. И у меня возникла идея. Маркетолог едва не поседел, когда её услышал. Он твердил, что с таким подходом я просто разорюсь, но я настоял на своём. <br><br><span style="color: #030917; font-size: 22px;">Итак…</span></p>
+				<div class="result" style="background: url(result.png) no-repeat; height: 120px; margin-top: 35px; margin-left: 80px;"></div>
+			</div>
+			<a href="#"class="button-2" style="display: block; background: url(buttonn.png); width:604px; height:63px; border-radius: 8px; -moz-border-radius: 8px; -o-border-radius: 8px; -webkit-border-radius: 8px; text-decoration: none; box-sizing:border-box; -moz-box-sizing:border-box; border-bottom:solid 2px #5c8a1a; font-family: sans-serif; color:  #ffffff; font-size: 18px; font-weight: 400; line-height: 18px; text-align: center; margin: 55px auto; padding-top: 23px">Получить скидку и пройти сегодняшнюю реинтеграцию</a>
+			<div class="signature" style="font-family: sans-serif; color: #030917; font-size: 22px; font-weight: 400; line-height: 12px; text-align: center; margin-top: 50px;">С любовью и уважением, <br><br><span style="font-family: sans-serif; color: #fa3b48; font-size: 20px; font-weight: 400; line-height: 12px; text-align: center;">Константин Довлатов</span></div>
+			<hr style="color: #f7f7f7; size: 1px; width: 575px; margin-top: 45px"></hr>
+			<div class="ps" style="color:  #030917; font-size: 14px; font-style: italic; letter-spacing: 0.35px; line-height: 22px; text-align: center; margin-top: 35px;">P.S. <span style="color:  #5c6770;">Чтобы получить скидку, <a href="#" style="color: #70bb02;">авторизуйтесь</a> на сайте “100 дней”</span></div>
+			<p style="font-family: sans-serif; color: #030917; font-size: 18px; font-weight: 400; line-height: 48px; text-align: center;">«<span style="font-size: 22px;">100</span> дней»</p>
+			<div class="bottom" style="background: url(bottom.png) no-repeat;"></div>
+		</div>
+	</body>
+</html>
